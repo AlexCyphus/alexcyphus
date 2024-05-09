26561a7248d1b1f1002262a2e57c3775eac9b05b
Alex Cyphus <alexjcyphus@gmail.com>
2024-05-10 00:47:27 +0200

    progress

--- app/_components/Desktop/Desktop.tsx
+++ app/_components/Desktop/Desktop.tsx
@@ -1,7 +1,60 @@
-import { Stack, Text } from "@chakra-ui/react";
+import { HStack, Stack, Text } from "@chakra-ui/react";
 import ApplicationIcon, { ApplicationIconProps } from "./ApplicationIcon";
 import TravelModal from "../Modals/TravelModal";
 import AboutModal from "../Modals/AboutModal";
+
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+const WaveText = () => {
+  return (
+    <Box
+      fontFamily="mono"
+      display="inline-block"
+      fontWeight={"bold"}
+      fontSize={"xx-large"}
+    >
+      <HStack>
+        {"welcome   to    alex    os".split("").map((letter, index) => (
+          <Box
+            as="span"
+            key={index}
+            display="inline-block"
+            animation={`wave 15s infinite ${
+              index * 0.2
+            }s cubic-bezier(0.36, 0.45, 0.63, 0.53)`}
+            transformOrigin="center"
+            mt="-5px"
+            mb="5px"
+          >
+            {letter}
+          </Box>
+        ))}
+      </HStack>
+      <style jsx global>{`
+        @keyframes wave {
+          0%,
+          100% {
+            transform: translateY(0);
+          }
+          10%,
+          30%,
+          50%,
+          70%,
+          90% {
+            transform: translateY(-10px);
+          }
+          20%,
+          40%,
+          60%,
+          80% {
+            transform: translateY(10px);
+          }
+        }
+      `}</style>
+    </Box>
+  );
+};
 
 const applications: ApplicationIconProps[] = [
   {
@@ -27,7 +80,7 @@ const applications: ApplicationIconProps[] = [
 const Desktop = () => {
   return (
     <Stack
-      bgColor={"gray"}
+      bgColor={"white"}
       h="100%"
       w="100%"
       maxHeight={"calc(100vh - 33px)"}
@@ -35,7 +88,7 @@ const Desktop = () => {
     >
       <AboutModal />
       <TravelModal />
-      <Text fontFamily={"sanfran"}>Welcome to AlexOS</Text>
+      <WaveText />
       <Stack p={4}>
         {applications.map((application) => (
           <ApplicationIcon key={application.id} {...application} />
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,18 @@ import App from "./_components/App";
 import Providers from "./_providers/Providers";
 
 // TO DO
-// PROJECTS APPLICATION
+// ----------------
+// APPLICATIONS
+// // PROJECTS AS A GRID OF ALL PROJECTS LIKE ON THE CURRENT WEBSITE
+// // HOBBIES AS A GRID OF HOBBIES
+// GITHUB LINK
+// ADD WOBBLIES EVERYWHERE
+// // BUTTON TO ADD MORE WOBBLIES
+// // BUTTON TO DELETE WOBBLIES
+// // BUTTON TO DELETE ALL WOBBLIES
+// URLS ON ABOUT ME
+// APPLICATION ICONS DRAGGABLE
+// REDESIGN
 // DEPLOY
 
 export default function Home() {
